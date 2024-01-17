4035e45b6162f496ff459965047d240d7e6d21bc
nightskyshop <nightskyshop1023@naver.com>
2024-01-17 15:30:16 +0900

    Style: Change Project Name to baseballtalk

--- src/main/java/com/example/test/BaseballtalkApplication.java
+++ src/main/java/com/example/test/BaseballtalkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class TestApplication {
+public class BaseballtalkApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(BaseballtalkApplication.class, args);
 	}
 }
